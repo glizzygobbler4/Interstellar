d4597a234fad1a907108b637671793f64575662b
glizzygobbler4 <132182374+glizzygobbler4@users.noreply.github.com>
2024-03-26 16:49:37 -0500

    Update index.js

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ const app = express(server)
 const bareServer = createBareServer('/o/')
 const PORT = process.env.PORT || 8080
 if (config.challenge) {
-  console.log('Password protection is enabled. Usernames are:educationalhelp ' + Object.keys(config.users))
-  console.log('Passwords are:lightspeedproxy' + Object.values(config.users))
+  console.log('Password protection is enabled. Usernames are: ' + Object.keys(config.users))
+  console.log('Passwords are:' + Object.values(config.users))
 
   app.use(
     basicAuth({
